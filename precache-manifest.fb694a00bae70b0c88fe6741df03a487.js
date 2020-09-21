bad52440dd9cd8b54b0b04d0cf4de576d673a279
jvracasa <johnvincentracasa@gmail.com>
2020-09-22 01:29:29 +0800

    Updates

---
+++ precache-manifest.fb694a00bae70b0c88fe6741df03a487.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "996e52bb1030b61d0c422b92ef56017f",
+    "url": "/jvracasa/github.io/bradsmart-ph-clone/index.html"
+  },
+  {
+    "revision": "1750b5df3a0e7a81aaa7",
+    "url": "/jvracasa/github.io/bradsmart-ph-clone/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c0476be786511176d306",
+    "url": "/jvracasa/github.io/bradsmart-ph-clone/static/js/2.13b8fe73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jvracasa/github.io/bradsmart-ph-clone/static/js/2.13b8fe73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1750b5df3a0e7a81aaa7",
+    "url": "/jvracasa/github.io/bradsmart-ph-clone/static/js/main.54914575.chunk.js"
+  },
+  {
+    "revision": "e715b8e02ebedc2d1b2f",
+    "url": "/jvracasa/github.io/bradsmart-ph-clone/static/js/runtime-main.b5067100.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jvracasa/github.io/bradsmart-ph-clone/static/media/logo.5d5d9eef.svg"
+  }
+]);
